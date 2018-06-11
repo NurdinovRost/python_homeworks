d4d9dfa45ec3fd698013461a79766970b62b0b66
NurdinovRost <nurdinov.rost@gmail.com>
2018-06-11 19:29:21 +0300

    Регистрация

--- elevennotes/src/config/settings/base.py
+++ elevennotes/src/config/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notes',
     'accounts',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -128,4 +129,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = '/static'
 
+STATICFILES_DIRS = [
+    root('static'),
+]
+
 AUTH_USER_MODEL = 'accounts.User'
+
+LOGIN_REDIRECT_URL = '/'
--- elevennotes/src/config/urls.py
+++ elevennotes/src/config/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='notes/'), name='index'),
+    
     path('notes/', include('notes.urls', namespace='notes')),
+    
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    
     path('admin/', admin.site.urls),
 ]
